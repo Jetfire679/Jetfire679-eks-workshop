f7025a16d3c35c5dc90c86deb21ea5b247dd675d
jetfire679 <jetfire679@gmail.com>
2023-04-17 10:18:15 -0400

    updates

--- hack/lib/terraform-context.sh
+++
@@ -1,7 +0,0 @@
-terraform_dir="$SCRIPT_DIR/../$terraform_context"
-
-export ASSUME_ROLE=$(terraform -chdir=$terraform_dir output -raw iam_role_arn)
-
-TEMP='/tmp/eks-workshop-shell-env'
-
-terraform -chdir=$terraform_dir output -raw environment_variables > $TEMP
--- website/docs/fundamentals/fargate/tests/hook-enabling.sh
+++
@@ -1,9 +0,0 @@
-before() {
-  echo "noop"
-}
-
-after() {
-  kubectl rollout status -n checkout deployment/checkout --timeout=200s
-}
-
-"$@"
